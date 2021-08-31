29a2faaa44a7eb72c28b8872c3c43287aac7db65
zacksteel <zlsteel@berkeley.edu>
2021-08-31 09:20:17 -0700

    tweeked fire-level figure

--- Code/fuel_v_weather.R
+++ Code/fuel_v_weather.R
@@ -139,13 +139,13 @@ p.a = filter(d3, term == "tslf") %>%
 ## Drought mortality
 p.b = filter(d3, term == "ads_mort") %>% 
   f1() +
-  coord_cartesian(ylim = c(-0.75, 0.75)) +
-  scale_y_continuous(breaks = c(log(0.5), 0, log(2)),
-                     labels = c(0.5 , 1, 2)) +
-  xlab("Drought Mortality - Effect Rank")
-  # coord_cartesian(ylim = c(-0.15, 0.4)) +
-  # scale_y_continuous(breaks = c(log(0.8), 0, log(1.25)),
-  #                    labels = c(0.8 , 1, 1.25))
+  # coord_cartesian(ylim = c(-0.75, 0.75)) +
+  # scale_y_continuous(breaks = c(log(0.5), 0, log(2)),
+  #                    labels = c(0.5 , 1, 2)) +
+  xlab("Drought Mortality - Effect Rank") +
+  coord_cartesian(ylim = c(-0.15, 0.4)) +
+  scale_y_continuous(breaks = c(log(0.8), 0, log(1.25)),
+                     labels = c(0.8 , 1, 1.25))
 
 ## VPD
 p.c = filter(d3, term == "vpd") %>% 
@@ -185,113 +185,113 @@ save_plot("Figures/FireEffects.png", p,
           base_width = 9, base_height = 7)
 
 
-pw = filter(d3, term %in% c('windspd', 'vpd', 'fm1000')) %>% 
-  ggplot(aes(y = lab, x = ep_median, 
-                    xmin = .lower, xmax = .upper,
-                    group = fire_na, fill = phs,
-                    point_size = ha)) +
-  ## Having trouble modifying legend for pointinterval; omitting
-  geom_pointinterval(position = position_dodge(width = .6),
-                     interval_size = 0.5, interval_alpha = 0.5,
-                     pch = 21,
-                     show.legend = c(point_size = F, size = F)) +
-  scale_fill_continuous_sequential("BurgYl",
-                                   name = "Percent\nHigh-severity",
-                                   breaks = c(.25, .5, .75),
-                                   labels = c(25, 50, 75),
-                                   alpha = 0.8) +
-  theme_bw() +
-  theme(panel.grid.minor = element_blank()) +
-  geom_vline(xintercept = 0, lty = 2, alpha = 0.75) +
-  coord_cartesian(xlim = c(-2.5, 5.5)) +
-  scale_x_continuous(breaks = c(log (0.25), 0, log(5), log(50)),
-                     labels = c(0.25, 1, 5, 50)) +
-  xlab("Relative effect (odds ratio)") + ylab(NULL) +
-  annotate("text",
-           x = c(0.85, 1.2, 5.2,
-                 -0.95, 1.5), 
-           y = c(3.35, 3.1, 3.2,
-                 2.35, 2.3),
-           label = c("North", "River", 'CZU',
-                     'Creek', 'Hennessey'),
-           size = 3)
-
-## find fires to label
-## ggrepel doesn't seem to work with dodge
-filter(d3, term %in% c('ads_mort')) %>% 
-  arrange(desc(ep_median)) %>% 
-  head
-filter(d3, term == "tslf") %>% 
-  arrange(ep_median) %>% 
-  slice(c(1, nrow(.)))
-filter(d3, term == "tslf") %>% 
-  arrange(desc(ha)) %>% 
-  head
-
-pf = filter(d3, term %in% c('tslf', 'ads_mort')) %>% 
-  ggplot(aes(y = lab, x = ep_median, 
-             xmin = .lower, xmax = .upper,
-             group = fire_na, label = fire_na,
-             fill = phs,
-             point_size = ha)) +
-  geom_pointinterval(position = position_dodge(width = .6),
-                     interval_size = 0.5, interval_alpha = 0.5,
-                     pch = 21,
-                     show.legend = c(point_size = F, size = F)) +
-  scale_fill_continuous_sequential("BurgYl",
-                                   name = "Percent\nHigh-severity",
-                                   breaks = c(.25, .5, .75),
-                                   labels = c(25, 50, 75),
-                                   alpha = 0.8) +
-  theme_bw() +
-  theme(panel.grid.minor = element_blank()) +
-  geom_vline(xintercept = 0, lty = 2, alpha = 0.75) +
-  coord_cartesian(xlim = c(-0.75, 0.75)) +
-  scale_x_continuous(breaks = c(log(0.5), 0, log(2)),
-                     labels = c(0.5, 1, 2)) +
-  xlab("Relative effect (odds ratio)") + ylab(NULL) + 
-  annotate("text", 
-           x = c(-1.1, -0.4, 0.25, 1, 
-                 .3, .35), 
-           y = c(2.25, 2.4, 2.42, 1.9, 
-                 1.125, 1.35), 
-           label = c('Dolan', 'SCU', 'August', 'Hog', 
-                     'Castle', 'Creek'),
-           size = 3)
-
-## patch and save
-layout = '
-AABB
-AABB
-AABB
-AACC'
-out = pw + pf + guide_area() +
-  plot_layout(design = layout, guides = 'collect') &
-  theme(legend.direction = "horizontal")
-
-save_plot("Figures/FireEffects.png", out,
-          base_width = 8, base_height = 6)
-
-
-## tslf vs. windspd
-p = ggplot(d2, aes(y = ep_median_tslf, x = ep_median_windspd, 
-                   ymin = .lower_tslf, ymax = .upper_tslf,
-                   xmin = .lower_windspd, xmax = .upper_windspd,
-                   label = fire_na)) +
-  geom_text_repel(size = 2) +
-  geom_pointinterval(interval_alpha = 0.3,
-                     interval_size = 2) +
-  geom_pointinterval(orientation = "horizontal",
-                     point_size = 2,
-                     interval_alpha = 0.3,
-                     interval_size = 2) +
-  geom_abline(lty = 2) +
-  theme_bw() +
-  theme(panel.grid.minor = element_blank()) +
-  ylab("TSLF Importance") + xlab("Wind Speed Importance") 
-
-save_plot("Figures/fuel_wind.png", p,
-          base_width = 8, base_height = 6)
+# pw = filter(d3, term %in% c('windspd', 'vpd', 'fm1000')) %>% 
+#   ggplot(aes(y = lab, x = ep_median, 
+#                     xmin = .lower, xmax = .upper,
+#                     group = fire_na, fill = phs,
+#                     point_size = ha)) +
+#   ## Having trouble modifying legend for pointinterval; omitting
+#   geom_pointinterval(position = position_dodge(width = .6),
+#                      interval_size = 0.5, interval_alpha = 0.5,
+#                      pch = 21,
+#                      show.legend = c(point_size = F, size = F)) +
+#   scale_fill_continuous_sequential("BurgYl",
+#                                    name = "Percent\nHigh-severity",
+#                                    breaks = c(.25, .5, .75),
+#                                    labels = c(25, 50, 75),
+#                                    alpha = 0.8) +
+#   theme_bw() +
+#   theme(panel.grid.minor = element_blank()) +
+#   geom_vline(xintercept = 0, lty = 2, alpha = 0.75) +
+#   coord_cartesian(xlim = c(-2.5, 5.5)) +
+#   scale_x_continuous(breaks = c(log (0.25), 0, log(5), log(50)),
+#                      labels = c(0.25, 1, 5, 50)) +
+#   xlab("Relative effect (odds ratio)") + ylab(NULL) +
+#   annotate("text",
+#            x = c(0.85, 1.2, 5.2,
+#                  -0.95, 1.5), 
+#            y = c(3.35, 3.1, 3.2,
+#                  2.35, 2.3),
+#            label = c("North", "River", 'CZU',
+#                      'Creek', 'Hennessey'),
+#            size = 3)
+# 
+# ## find fires to label
+# ## ggrepel doesn't seem to work with dodge
+# filter(d3, term %in% c('ads_mort')) %>% 
+#   arrange(desc(ep_median)) %>% 
+#   head
+# filter(d3, term == "tslf") %>% 
+#   arrange(ep_median) %>% 
+#   slice(c(1, nrow(.)))
+# filter(d3, term == "tslf") %>% 
+#   arrange(desc(ha)) %>% 
+#   head
+# 
+# pf = filter(d3, term %in% c('tslf', 'ads_mort')) %>% 
+#   ggplot(aes(y = lab, x = ep_median, 
+#              xmin = .lower, xmax = .upper,
+#              group = fire_na, label = fire_na,
+#              fill = phs,
+#              point_size = ha)) +
+#   geom_pointinterval(position = position_dodge(width = .6),
+#                      interval_size = 0.5, interval_alpha = 0.5,
+#                      pch = 21,
+#                      show.legend = c(point_size = F, size = F)) +
+#   scale_fill_continuous_sequential("BurgYl",
+#                                    name = "Percent\nHigh-severity",
+#                                    breaks = c(.25, .5, .75),
+#                                    labels = c(25, 50, 75),
+#                                    alpha = 0.8) +
+#   theme_bw() +
+#   theme(panel.grid.minor = element_blank()) +
+#   geom_vline(xintercept = 0, lty = 2, alpha = 0.75) +
+#   coord_cartesian(xlim = c(-0.75, 0.75)) +
+#   scale_x_continuous(breaks = c(log(0.5), 0, log(2)),
+#                      labels = c(0.5, 1, 2)) +
+#   xlab("Relative effect (odds ratio)") + ylab(NULL) + 
+#   annotate("text", 
+#            x = c(-1.1, -0.4, 0.25, 1, 
+#                  .3, .35), 
+#            y = c(2.25, 2.4, 2.42, 1.9, 
+#                  1.125, 1.35), 
+#            label = c('Dolan', 'SCU', 'August', 'Hog', 
+#                      'Castle', 'Creek'),
+#            size = 3)
+# 
+# ## patch and save
+# layout = '
+# AABB
+# AABB
+# AABB
+# AACC'
+# out = pw + pf + guide_area() +
+#   plot_layout(design = layout, guides = 'collect') &
+#   theme(legend.direction = "horizontal")
+# 
+# save_plot("Figures/FireEffects.png", out,
+#           base_width = 8, base_height = 6)
+# 
+# 
+# ## tslf vs. windspd
+# p = ggplot(d2, aes(y = ep_median_tslf, x = ep_median_windspd, 
+#                    ymin = .lower_tslf, ymax = .upper_tslf,
+#                    xmin = .lower_windspd, xmax = .upper_windspd,
+#                    label = fire_na)) +
+#   geom_text_repel(size = 2) +
+#   geom_pointinterval(interval_alpha = 0.3,
+#                      interval_size = 2) +
+#   geom_pointinterval(orientation = "horizontal",
+#                      point_size = 2,
+#                      interval_alpha = 0.3,
+#                      interval_size = 2) +
+#   geom_abline(lty = 2) +
+#   theme_bw() +
+#   theme(panel.grid.minor = element_blank()) +
+#   ylab("TSLF Importance") + xlab("Wind Speed Importance") 
+# 
+# save_plot("Figures/fuel_wind.png", p,
+#           base_width = 8, base_height = 6)
 
 ## Table of median values
 arrange(d, median) %>% 
